2108e43b04ec471ea14b84355eb5ba6b2cee7af6
Zin Lin Phyo <zinlinphyo.mm@gmail.com>
2018-08-01 17:39:48 +0630

    init commit

---
+++ app/src/main/java/net/zinlinphyo/mmkunyi/MMKuNyiApp.kt
@@ -0,0 +1,9 @@
+package net.zinlinphyo.mmkunyi
+
+import android.app.Application
+
+class MMKuNyiApp : Application() {
+	override fun onCreate() {
+		super.onCreate()
+	}
+}
---
+++ app/src/main/java/net/zinlinphyo/mmkunyi/activities/JobListActivity.kt
@@ -0,0 +1,13 @@
+package net.zinlinphyo.mmkunyi.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.zinlinphyo.mmkunyi.R
+
+class JobListActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_job_list)
+	}
+}
